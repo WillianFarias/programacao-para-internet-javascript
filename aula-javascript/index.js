a4a0941484a60105f9de57f7c16abd45fb51713c
WillianFarias <williansousafarias@gmail.com>
2020-12-28 21:35:42 -0300

    usando console.log e replace

--- aula-javascript/index.js
+++ aula-javascript/index.js
@@ -1,4 +1,6 @@
 var nome = "Willian Sousa Farias";
 var idade = 27;
 
-alert(`Bem vindo ${nome}, sua idade é ${idade} anos`);
+//alert(`Bem vindo ${nome}, sua idade é ${idade} anos`);
+console.log(nome.replace("Willian", "Samanta"));
+//console.log(nome);
